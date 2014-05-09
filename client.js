a50b53d0dc1c982a6822dd75d70099eabad81766
Heark <hearkofsorcerey@gmail.com>
2014-05-09 13:39:20 -0400

    if kick message

---
+++ client.js
@@ -0,0 +1,16 @@
+ ({
+        afterChannelMessage: function (
+            message, channel, html) {
+            if (message.indexOf(':') >=
+                0) {
+                msg = message.substr(
+                    message.indexOf(':') +
+                    2);
+            }
+ 
+ function playerKicked(msg, channel);
+ client.network().sendChanMessage(
+                    channel,
+                    " HA!, Kick him again."
+                );
+  
